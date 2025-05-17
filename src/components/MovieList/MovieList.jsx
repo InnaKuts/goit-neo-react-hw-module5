159c8af755152907dad4c7ebb9259a31048bb513
Inna Kuts <inna.v.kuts@gmail.com>
2025-05-17 14:29:26 +0200

    extracted movie card

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,6 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import MovieCard from "../MovieCard/MovieCard";
 import styles from "./MovieList.module.css";
-import { getImageUrl } from "../../services/apiService";
 
 const MovieList = ({ movies }) => {
   const location = useLocation();
@@ -12,32 +12,7 @@ const MovieList = ({ movies }) => {
   return (
     <ul className={styles.movieList}>
       {movies.map((movie) => (
-        <li key={movie.id} className={styles.movieItem}>
-          <Link
-            to={`/movies/${movie.id}`}
-            state={{ from: location }}
-            className={styles.movieLink}
-          >
-            <div className={styles.moviePoster}>
-              {movie.poster_path ? (
-                <img
-                  src={getImageUrl(movie.poster_path)}
-                  alt={movie.title}
-                  className={styles.posterImage}
-                />
-              ) : (
-                <div className={styles.noPoster}>No poster available</div>
-              )}
-            </div>
-            <div className={styles.movieInfo}>
-              <h3 className={styles.movieTitle}>{movie.title}</h3>
-              <p className={styles.movieDate}>
-                {movie.release_date &&
-                  new Date(movie.release_date).getFullYear()}
-              </p>
-            </div>
-          </Link>
-        </li>
+        <MovieCard key={movie.id} movie={movie} location={location} />
       ))}
     </ul>
   );
