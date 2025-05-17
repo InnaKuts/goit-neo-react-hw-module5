92069622b70988c76e75b2d43a46c3c6c4ab3566
Inna Kuts <inna.v.kuts@gmail.com>
2025-05-17 13:35:25 +0200

    dummy components

---
+++ src/components/MovieCast/MovieCast.jsx
@@ -0,0 +1,11 @@
+import styles from "./MovieCast.module.css";
+
+const MovieCast = () => {
+  return (
+    <div className={styles.cast}>
+      <p>Movie cast will be implemented here</p>
+    </div>
+  );
+};
+
+export default MovieCast;
---
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -0,0 +1,11 @@
+import styles from "./MovieReviews.module.css";
+
+const MovieReviews = () => {
+  return (
+    <div className={styles.reviews}>
+      <p>Movie reviews will be implemented here</p>
+    </div>
+  );
+};
+
+export default MovieReviews;
